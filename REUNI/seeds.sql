ea43e3c9b9d72a215f7aa830e1e5293988668e0c
JimboKirkland <31573144+JimboKirkland@users.noreply.github.com>
2018-03-15 21:39:12 -0400

    Add files via upload

---
+++ REUNI/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO reunifyDB (StudentID, StudentName, StudentPhoto, GradeLevel, HomeroomTeacher, DriversID, ParentName, ParentName2) VALUES (9200904675, 'John Smith', 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX1698294.jpg', 3, 'Mrs. Thigpen', 076523847, 'John Smith Sr.', 'Jane Smith' );
