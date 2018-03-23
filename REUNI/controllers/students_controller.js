164d628305ee0e37a112115d7f81274d66af9fb7
kalaunlimited <KLasode@gmail.com>
2018-03-23 02:41:18 -0400

    database is linked and routes are linked

--- REUNI/controllers/students_controller.js
+++ REUNI/controllers/students_controller.js
@@ -7,6 +7,7 @@ var express = require("express");
 var router = express.Router();
 // grabbing our models
 var db = require("../models");
+var body= require("body-parser");
 
 // get route -> index
 router.get("/", function(req, res) {
@@ -17,11 +18,11 @@ router.get("/", function(req, res) {
 
 
 // get route -> index, edited to match sequelize
-
+//this get method is for the modal
 router.get("/students", function(req, res) {
   db.Students.findAll({
     where: {
-      student_id = req.body.student_id
+      student_id :req.body.student_id
     },
   })
   // use promise method to pass the students...
@@ -38,73 +39,33 @@ router.get("/students", function(req, res) {
 });
 
 
-// get route -> index, edited to match sequelize
-router.get("/students/reunify_point", function(req, res) {
-  db.Reunify_points.findOne({
-    where: {
-      reunify_point_count: {[Op.lt]: 20}
-    },
-  })
-  // use promise method to pass the students...
-  .then(function(dbReunify_points) {
 
 
 
-router.get("/checkin", function(req, res) {
 
 
-    console.log("students_reunify_point_find: ", dbReunify_points);
-
-    // into the main index, updating the page
-    var PugObject = {
-      reunify_point: dbReunify_points
-    };
-    return res.render("index", PugObject);
-  });
-});
 
 
-// put route to update   
-
-    return res.render("parent", {title: "Parent"});
-});
-router.get("/admin", function(req, res) {
-
-    return res.render("admin", {title: "Admin"});
-});
-router.get("/result", function(req, res) {
-
-    return res.render("result", {title: "Results"});
-});
-router.get("/index", function(req, res) {
 
 
-    return res.render("index", {title: "index"});
-});
 router.put("/students/update", function(req, res) {
   
-  if (req.body.student.status != 'Released' &&  ([string]::IsNullOrEmpty(req.body.reunify_pnt))) {    
 
-    db.Reunify_points.update({
-      reunify_point_count++
-      where: {
-        reunify_point: req.body.reunify_point
-      },
+     db.Students.update({
+      
+      student_status : 'CHECKED'
     })
 
-    }
+    
     .then(function(dbReunify_points) {
 
       console.log("student_update_reunify_point: ", dbReunify_points);
 
-      return db.Students.update({
-        reunify_pnt: req.body.reunify_point,
-        student_status = 'Received'
-      })
-    .then(function(dbStudents) {
-      res.redirect("/students");
-    });
-  }
+      
+    
+      res.redirect("/");
+   
+  });
 });
 
 module.exports = router;
--- REUNI/server.js
+++ REUNI/server.js
@@ -20,30 +20,42 @@ app.use(methodOverride("_method"));
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+// var routes1 = require("./controllers/students_controller");
+// var routes2 = require("./controllers/checkin_parent_controller");
+// var routes3 = require("./controllers/admin_students_controller");
+// var routes4 = require("./controllers/admin_reunify_controller");
+// var routes5 = require("./controllers/admin_parents_controller");
 
-var routes1 = require("./controllers/students_controller");
-var routes2 = require("./controllers/checkin_parent_controller");
-var routes3 = require("./controllers/admin_students_controller");
-var routes4 = require("./controllers/admin_reunify_controller");
-var routes5 = require("./controllers/admin_parents_controller");
 
-app.use("/", routes1);
-app.use("/students", routes1);
-app.use("/students/update", routes1);
-app.use("/students/reunify_point", routes1);
+//dereks routes
 
-app.use("/checkin_parent", routes2);
-app.use("/checkin_parent/create", routes2);
+// app.use("/", routes1);
+// app.use("/update", routes1);
+// app.use("/create", routes1);
+//app.use("/checkin", routes);
 
-app.use("/admin_students", routes3);
+// app.use("/", routes1);
+// app.use("/students", routes1);
+// app.use("/students/update", routes1);
+// app.use("/students/reunify_point", routes1);
 
-app.use("/admin_reunify", routes4);
-app.use("/admin_reunify/create", routes4);
+// app.use("/checkin_parent", routes2);
+// app.use("/checkin_parent/create", routes2);
 
-app.use("/admin_parents", routes5);
+// app.use("/admin_students", routes3);
+
+// app.use("/admin_reunify", routes4);
+// app.use("/admin_reunify/create", routes4);
+
+// app.use("/admin_parents", routes5);
+
+app.use(require('./controllers/students'))
+app.use(require('./controllers/view-routes'));
 
 // listen on port 3000
 var port = process.env.PORT || 3000;
-db.sequelize.sync().then(function() {
-  app.listen(port);
+// db.sequelize.sync().then(function() {
+// });
+app.listen(port, function() {
+  console.log('listening on port ' + port);
 });
